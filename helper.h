f676a0f26c8d5b7145fe84c45e118fbddaaeddb5
Athena <athena.bonzel@proton.me>
2025-05-18 14:08:03 +0200

    Added License terms to every source file

--- helper.h
+++ helper.h
@@ -1,3 +1,29 @@
+/*
+
+    LICENSE
+
+    'recipe-calc', a simple crafting chain calculator.
+    Copyright (C) 2025 https://github.com/AthenaCannotCode
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  It can be found in the file LICENSE.
+    If not, see <https://www.gnu.org/licenses/>.
+
+    Contact: E-Mail: athena.bonzel@proton.me
+
+*/
+
+
 //
 // Created by athena on 16.05.25.
 //
